ab98a205c02450f9f1df206674bd7c4b53af797b
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-03 00:45:22 -0700

    created state slice and reducers for icecream

---
+++ features/icecream/icecreamSlice.js
@@ -0,0 +1,21 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+
+const initialState = {
+  numIcecreams: 5
+}
+
+const icecreamSlice = createSlice({
+  name: icecream,
+  initialState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numIcecreams -= action.payload;
+    },
+    restocked: (state, action) => {
+      state.numIcecreams += action.payload;
+    }
+  }
+});
+
+module.exports = icecreamSlice.reducer;
+module.exports.icecreamActions = icecreamSlice.actions;
