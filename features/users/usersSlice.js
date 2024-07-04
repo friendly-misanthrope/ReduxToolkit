8a3beb54def8944cdf1a9844bef1c6ab1721f49d
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-03 19:13:29 -0700

    created fetchUsers async thunk action creator

--- features/users/usersSlice.js
+++ features/users/usersSlice.js
@@ -1,8 +1,19 @@
 const createSlice = require('@reduxjs/toolkit').createSlice;
-const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk;
+const axios = require('axios');
 
 const initialState = {
   loading: false,
   users: [],
   error: ''
 }
+
+// Promise resolves to pending, fulfilled, or rejected action type
+const fetchUsers = createAsyncThunk('users/fetchUsers', () => {
+  axios.get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => {
+      res.data.map((user) => {
+        return user
+      });
+    });
+});
