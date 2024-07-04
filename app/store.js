50836711af792d3ccf2d8214ff36982ea845ff44
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-03 19:33:38 -0700

    imported usersReducer and added to configureStore()

--- app/store.js
+++ app/store.js
@@ -2,13 +2,15 @@ const configureStore = require('@reduxjs/toolkit').configureStore;
 const reduxLogger = require('redux-logger');
 const cakeReducer = require('../features/cake/cakeSlice').reducer;
 const icecreamReducer = require('../features/icecream/icecreamSlice').reducer;
+const usersReducer = require('../features/users/usersSlice').reducer;
 
 const logger = reduxLogger.createLogger();
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
-    icecream: icecreamReducer
+    icecream: icecreamReducer,
+    users: usersReducer
   },
   middleware: ((getDefaultMiddleware) => {
     return getDefaultMiddleware().concat(logger)
