01dcb568ec66fee373b5d41f331cef18cbe18e39
friendly-misanthrope <nickthedevguy@gmail.com>
2024-07-03 18:40:53 -0700

    Added state slice for users, installed & imported deps, declared initalState

---
+++ features/users/usersSlice.js
@@ -0,0 +1,8 @@
+const createSlice = require('@reduxjs/toolkit').createSlice;
+const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: ''
+}
